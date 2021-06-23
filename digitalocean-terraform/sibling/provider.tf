4479cc064067f5c724d52aad892b8da7b08749c8
Will Ruggiano <wmruggiano@gmail.com>
2021-06-23 00:26:07 -0400

    Separate the genesis node infra from the sibling nodes

--- digitalocean-terraform/sibling/provider.tf
+++ digitalocean-terraform/sibling/provider.tf
@@ -13,16 +13,29 @@ data "digitalocean_ssh_key" "barbosa-ssh-key" {
   name = "barbosa"
 }
 
+variable "node_count" {
+  type    = number
+  default = 1
+}
+
+variable "beaconchain_id" {
+  type = string
+}
+
 resource "digitalocean_volume" "beaconchain" {
-  region      = "nyc1"
-  name        = "beaconchain"
-  size        = 100
-  description = "file system storage for beacon-chain data"
+  count                    = var.node_count
+  region                   = "nyc1"
+  name                     = "beaconchain-${count.index}"
+  size                     = 100
+  description              = "file system storage for beacon-chain data"
+  snapshot_id              = var.beaconchain_id
+  initial_filesystem_label = "beaconchain"
 }
 
 resource "digitalocean_droplet" "node" {
+  count      = var.node_count
   image      = "ubuntu-20-04-x64"
-  name       = "genesis-node"
+  name       = "sibling-node-${count.index}"
   region     = "nyc1"
   size       = "s-4vcpu-8gb-intel"
   monitoring = true
@@ -30,23 +43,24 @@ resource "digitalocean_droplet" "node" {
     data.digitalocean_ssh_key.barbosa-ssh-key.id
   ]
   user_data = templatefile("${path.module}/../common/cloud-init.yaml", {
-    beaconchain_volume_name = "beaconchain"
+    beaconchain_volume_name = "beaconchain-${count.index}"
   })
 }
 
 resource "digitalocean_volume_attachment" "node" {
-  droplet_id = digitalocean_droplet.node.id
-  volume_id  = digitalocean_volume.beaconchain.id
+  count      = var.node_count
+  droplet_id = digitalocean_droplet.node[count.index].id
+  volume_id  = digitalocean_volume.beaconchain[count.index].id
 }
 
-output "instance_ip_addr" {
-  value = digitalocean_droplet.node.ipv4_address
+output "instance_ip_addrs" {
+  value = digitalocean_droplet.node.*.ipv4_address
 }
 
 resource "digitalocean_firewall" "node" {
-  name = "genesis-mainnet"
+  name = "sibling-mainnet"
 
-  droplet_ids = [digitalocean_droplet.node.id]
+  droplet_ids = digitalocean_droplet.node.*.id
 
   inbound_rule {
     protocol         = "tcp"
@@ -80,9 +94,7 @@ resource "digitalocean_firewall" "node" {
 }
 
 resource "digitalocean_project" "project" {
-  name = "genesis-mainnet"
-  resources = [
-    digitalocean_droplet.node.urn
-  ]
+  name      = "sibling-mainnet"
+  resources = digitalocean_droplet.node.*.urn
 }
 
