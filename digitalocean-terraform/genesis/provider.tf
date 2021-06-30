57e664c80c1813dd46ea4323d7bbf6ae1c3ddf8c
Will Ruggiano <wmruggiano@gmail.com>
2021-06-29 23:01:03 -0400

    Compose updates

--- digitalocean-terraform/genesis/provider.tf
+++ digitalocean-terraform/genesis/provider.tf
@@ -9,8 +9,16 @@ terraform {
 
 provider "digitalocean" {}
 
-data "digitalocean_ssh_key" "barbosa-ssh-key" {
+data "digitalocean_ssh_key" "barbosa" {
   name = "barbosa"
+}
+
+data "digitalocean_ssh_key" "blackbeard" {
+  name = "blackbeard"
+}
+
+data "digitalocean_ssh_key" "ljs" {
+  name = "ljs"
 }
 
 resource "digitalocean_volume" "beaconchain" {
@@ -20,6 +28,13 @@ resource "digitalocean_volume" "beaconchain" {
   description = "file system storage for beacon-chain data"
 }
 
+resource "digitalocean_volume" "eth1" {
+  region      = "nyc1"
+  name        = "eth1"
+  size        = 100
+  description = "file system storage for eth1 chain data"
+}
+
 resource "digitalocean_droplet" "node" {
   image      = "ubuntu-20-04-x64"
   name       = "genesis-node"
@@ -27,14 +42,27 @@ resource "digitalocean_droplet" "node" {
   size       = "s-4vcpu-8gb-intel"
   monitoring = true
   ssh_keys = [
-    data.digitalocean_ssh_key.barbosa-ssh-key.id
+    data.digitalocean_ssh_key.barbosa.id,
+    data.digitalocean_ssh_key.blackbeard.id,
+    data.digitalocean_ssh_key.ljs.id
   ]
-  user_data = file("${path.module}/../common/cloud-init.yaml")
+  user_data = templatefile("${path.module}/../common/cloud-init.yaml", {
+    barbosa_ssh_key    = data.digitalocean_ssh_key.barbosa.public_key,
+    blackbeard_ssh_key = data.digitalocean_ssh_key.blackbeard.public_key,
+    ljs_ssh_key        = data.digitalocean_ssh_key.ljs.public_key,
+    eth2_mount_name    = "beaconchain",
+    eth1_mount_name    = "eth1"
+  })
 }
 
-resource "digitalocean_volume_attachment" "node" {
+resource "digitalocean_volume_attachment" "eth2" {
   droplet_id = digitalocean_droplet.node.id
   volume_id  = digitalocean_volume.beaconchain.id
+}
+
+resource "digitalocean_volume_attachment" "eth1" {
+  droplet_id = digitalocean_droplet.node.id
+  volume_id  = digitalocean_volume.eth1.id
 }
 
 output "instance_ip_addr" {
